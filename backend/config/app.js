c17b10c44219fa4daebe3768985fb0cb356721f8
Pepi Petroff <74186271+PepiPetrov@users.noreply.github.com>
2021-12-15 15:34:50 +0200

    Added changes

--- backend/config/app.js
+++ backend/config/app.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const compression = require('compression')
 const app = express()
 
 app.use(express.json({ extended: true }))
 
 app.use(express.static('public'))
+
+app.use(compression())
 
 module.exports = app
